82790ffee217541835c1fcef30cc0badbcec9535
Xindref <dakotahmccrary@gmail.com>
2023-03-14 20:55:07 -0500

    fixed version number for Jasmine in index.html, and added test for updateServerTable()

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,4 +1,4 @@
-describe("Servers test (with setup and tear-down)", function() {
+describe("Servers test (with setup and tear-down)", function () {
   beforeEach(function () {
     // initialization logic
     serverNameInput.value = 'Alice';
@@ -11,7 +11,14 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
-  afterEach(function() {
-    // teardown logic
+  it('should make changes to server table on updateServerTable()', function () {
+    serverNameInput.value = 'Alice';
+    submitServerInfo();
+    expect(serverTbody.innerHTML).not.toEqual('');
+  })
+
+  afterEach(function () {
+    allServers = {};
+    updateServerTable();
   });
 });
